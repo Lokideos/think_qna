ee05db25dcd959179bf810422b080464fc5a0ce8
lokideos <archmage09@gmail.com>
2018-12-16 20:08:22 +0300

    Add null constraints to answers & questions to DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_16_170130) do
+ActiveRecord::Schema.define(version: 2018_12_16_170542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "answers", force: :cascade do |t|
-    t.string "body"
+    t.string "body", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "question_id"
@@ -24,8 +24,8 @@ ActiveRecord::Schema.define(version: 2018_12_16_170130) do
   end
 
   create_table "questions", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
+    t.string "title", null: false
+    t.text "body", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
