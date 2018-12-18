484372ff126f99ddbc3a17a43c26f94f4d52162a
lokideos <archmage09@gmail.com>
2018-12-18 21:22:23 +0300

    Question create action tests & realization

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -15,7 +15,21 @@ class QuestionsController < ApplicationController
 
   def edit; end
 
+  def create
+    @question = Question.new(question_params)
+
+    if @question.save
+      redirect_to @question
+    else
+      render :new
+    end
+  end
+
   private
+
+  def question_params
+    params.require(:question).permit(:title, :body)
+  end
 
   def load_question
     @question = Question.find(params[:id])
