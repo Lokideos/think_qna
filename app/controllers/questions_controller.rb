ea6acd959ceabb0a4916ea118be992340856e7ac
Lokideos <archmage09@gmail.com>
2018-12-20 01:32:40 +0300

    Remove before_action for question controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,17 +1,13 @@
 # frozen_string_literal: true
 
 class QuestionsController < ApplicationController
-  before_action :load_question, only: %i[show edit update destroy]
-
   def index
     @questions = Question.all
   end
 
   def show; end
 
-  def new
-    @question = Question.new
-  end
+  def new; end
 
   def edit; end
 
@@ -26,7 +22,7 @@ class QuestionsController < ApplicationController
   end
 
   def update
-    if @question.update(question_params)
+    if question.update(question_params)
       redirect_to @question
     else
       render :edit
@@ -34,7 +30,7 @@ class QuestionsController < ApplicationController
   end
 
   def destroy
-    @question.destroy
+    question.destroy
     redirect_to questions_path
   end
 
@@ -44,7 +40,9 @@ class QuestionsController < ApplicationController
     params.require(:question).permit(:title, :body)
   end
 
-  def load_question
-    @question = Question.find(params[:id])
+  def question
+    @question ||= params[:id] ? Question.find(params[:id]) : Question.new
   end
+
+  helper_method :question
 end
