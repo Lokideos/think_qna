a0941caacd37a409cd39ea63c6e4ce8d8325b8e8
Lokideos <archmage09@gmail.com>
2018-12-20 18:22:45 +0300

    Use memorization for question create action

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -14,7 +14,7 @@ class QuestionsController < ApplicationController
   def create
     @question = Question.new(question_params)
 
-    if @question.save
+    if question.save
       redirect_to @question
     else
       render :new
