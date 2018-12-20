adbe99f7dddcd99c47538f054599746a87bee0ac
Lokideos <archmage09@gmail.com>
2018-12-20 18:27:25 +0300

    Use memorization for loading needed question

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 class AnswersController < ApplicationController
-  before_action :load_question, only: %i[create]
-  before_action :load_related_question, only: %i[update destroy]
-
   def index
     @answers = Answer.all
   end
@@ -15,10 +12,10 @@ class AnswersController < ApplicationController
   def edit; end
 
   def create
-    @answer = @question.answers.new(answer_params)
+    @answer = question.answers.new(answer_params)
 
     if @answer.save
-      redirect_to @question
+      redirect_to question
     else
       render :new
     end
@@ -26,7 +23,7 @@ class AnswersController < ApplicationController
 
   def update
     if answer.update(answer_params)
-      redirect_to @question
+      redirect_to question
     else
       render :edit
     end
@@ -35,21 +32,17 @@ class AnswersController < ApplicationController
   def destroy
     answer.destroy
 
-    redirect_to @question
+    redirect_to question
   end
 
   private
 
-  def load_related_question
-    @question = answer.question
-  end
-
-  def load_question
-    @question = Question.find(params[:question_id])
+  def question
+    @question ||= params[:question_id] ? Question.find(params[:question_id]) : answer.question
   end
 
   def answer
-    @answer ||= params[:id] ? Answer.find(params[:id]) : load_question.answers.new
+    @answer ||= params[:id] ? Answer.find(params[:id]) : question.answers.new
   end
 
   def answer_params
