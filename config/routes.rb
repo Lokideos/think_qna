ee32b9418dc4140a945e98ab0db98af0b3720498
lokideos <archmage09@gmail.com>
2018-12-23 12:49:27 +0300

    Basic devise setup for the project & basic user sign in acceptance tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :questions do
     resources :answers, shallow: true
   end
+
+  root to: 'questions#index'
 end
