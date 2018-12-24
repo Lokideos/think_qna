9bb5b8672b83f26b81a2e914f69e55c62c3de7cc
Lokideos <archmage09@gmail.com>
2018-12-24 15:32:04 +0300

    Show sign in link to not logged in user

--- spec/features/user/sign_in_spec.rb
+++ spec/features/user/sign_in_spec.rb
@@ -20,6 +20,17 @@ feature 'User can sign in', "
     expect(page).to have_content 'Signed in successfully.'
   end
 
+  scenario 'Registered user tries to sign in from root page' do
+    visit root_path
+    click_on 'Sign in'
+
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_on 'Log in'
+
+    expect(page).to have_content 'Signed in successfully.'
+  end
+
   scenario 'Unregistered user tries to sign in' do
     fill_in 'Email', with: 'wrong@test.com'
     fill_in 'Password', with: '12345678'
