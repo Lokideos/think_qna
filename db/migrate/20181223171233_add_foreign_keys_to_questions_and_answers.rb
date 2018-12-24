854b3852a737513049b5a2f854af20e4a218c0a4
Lokideos <archmage09@gmail.com>
2018-12-24 15:34:33 +0300

    Check indexes on db level (add foreign_key options)

--- db/migrate/20181223171233_add_foreign_keys_to_questions_and_answers.rb
+++ db/migrate/20181223171233_add_foreign_keys_to_questions_and_answers.rb
@@ -2,7 +2,7 @@
 
 class AddForeignKeysToQuestionsAndAnswers < ActiveRecord::Migration[5.2]
   def change
-    add_belongs_to :questions, :user
-    add_belongs_to :answers, :user
+    add_belongs_to :questions, :user, foreign_key: true
+    add_belongs_to :answers, :user, foreign_key: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 2018_12_23_171233) do
   enable_extension "plpgsql"
 
   create_table "answers", force: :cascade do |t|
-    t.string "body", null: false
+    t.text "body", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "question_id"
@@ -46,4 +46,6 @@ ActiveRecord::Schema.define(version: 2018_12_23_171233) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "answers", "users"
+  add_foreign_key "questions", "users"
 end
