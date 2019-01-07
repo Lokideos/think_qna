c6706f8689760699bf94edfe35fe8a9b4d5841e3
lokideos <archmage09@gmail.com>
2019-01-07 13:29:21 +0300

    User can choose best answer, when other answer is already the best scenario

--- spec/features/question/choose_best_answer_spec.rb
+++ spec/features/question/choose_best_answer_spec.rb
@@ -2,6 +2,7 @@
 
 require 'rails_helper'
 
+# rubocop:disable Metrics/BlockLength
 feature 'User can choose best answer', "
   In order to clarify what answer was the most correct one
   As a User
@@ -10,24 +11,39 @@ feature 'User can choose best answer', "
   given(:user) { create(:user) }
   given(:non_author) { create(:user) }
   given(:question) { create(:question, user: user) }
+  given(:other_question) { create(:question, user: non_author) }
   given!(:answer) { create(:answer, question: question, user: non_author) }
 
   context 'Authenticated user' do
+    background { sign_in(user) }
+
     scenario 'choose best answer', js: true do
-      sign_in(user)
       visit question_path(question)
 
       within '.answers' do
         click_on 'Best Answer'
       end
 
-      expect(page).to have_content 'You chose best answer.'
+      expect(page).to have_content 'You chose the best answer.'
       within '.best-answer' do
         expect(page).to have_content answer.body
       end
     end
 
-    scenario 'tries to choose best answer, when the question already has best answer'
+    scenario 'tries to choose best answer, when the question already has best answer', js: true do
+      visit question_path(question)
+      create(:answer, body: 'Other Answer Body', question: question, best: true)
+
+      within '.answers' do
+        click_on(id: "choose-best-answer-link-#{answer.id}")
+      end
+
+      expect(page).to have_content 'You chose the best answer.'
+      within '.best-answer' do
+        expect(page).to have_content answer.body
+      end
+    end
+
     scenario "tries to choose best answer for other user's question"
   end
 
@@ -35,3 +51,4 @@ feature 'User can choose best answer', "
     scenario 'tries to choose best answer'
   end
 end
+# rubocop:enable Metrics/BlockLength
