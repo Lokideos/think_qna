6668a4170cf6512765c2e59c65a65b0138cf9fa1
Lokideos <archmage09@gmail.com>
2019-01-09 18:46:39 +0300

    Resolve n+1 for files attched to question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -46,7 +46,7 @@ class QuestionsController < ApplicationController
   end
 
   def question
-    @question ||= params[:id] ? Question.find(params[:id]) : Question.new
+    @question ||= params[:id] ? Question.with_attached_files.find(params[:id]) : Question.new
   end
 
   helper_method :question
