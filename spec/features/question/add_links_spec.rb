8165c90d9568b7742a49ecf3e7ebdee66974ba62
Lokideos <archmage09@gmail.com>
2019-01-13 21:51:38 +0300

    User can add multiplie links to question when he creates it

--- spec/features/question/add_links_spec.rb
+++ spec/features/question/add_links_spec.rb
@@ -2,6 +2,7 @@
 
 require 'rails_helper'
 
+# rubocop:disable Metrics/BlockLength
 feature 'User can add links to the question', "
   In order to provide additional information to my question
   As Author of the question
@@ -9,6 +10,7 @@ feature 'User can add links to the question', "
 " do
   given(:user) { create(:user) }
   given(:gist_url) { 'https://gist.github.com/Lokideos/815f3eea3f00a35ff48ea2984457b673' }
+  given(:google_url) { 'https://google.ru' }
 
   scenario 'User adds link when he asks the question' do
     sign_in(user)
@@ -24,4 +26,31 @@ feature 'User can add links to the question', "
 
     expect(page).to have_link 'My gist', href: gist_url
   end
+
+  scenario 'User adds several links when he asks the question', js: true do
+    sign_in(user)
+    visit new_question_path
+
+    fill_in 'Title', with: 'Test question'
+    fill_in 'Body', with: 'text text text'
+
+    fill_in 'Title', with: 'Test question'
+    fill_in 'Body', with: 'text text text'
+
+    fill_in 'Link name', with: 'My gist'
+    fill_in 'Url', with: gist_url
+
+    click_on 'add link'
+
+    within '.nested-fields' do
+      fill_in 'Link name', with: 'Google'
+      fill_in 'Url', with: google_url
+    end
+
+    click_on 'Ask'
+
+    expect(page).to have_link 'My gist', href: gist_url
+    expect(page).to have_link 'Google', href: google_url
+  end
 end
+# rubocop:enable Metrics/BlockLength
