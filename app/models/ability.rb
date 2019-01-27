4d4e876adc31f8d3825450e3e77466edcc0f3633
lokideos <archmage09@gmail.com>
2019-01-27 22:15:21 +0300

    Add authorization properties to questions, answers, comments and rating

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,9 +1,15 @@
 # frozen_string_literal: true
 
+# rubocop:disable Metrics/AbcSize
+# rubocop:disable Metrics/MethodLength
 class Ability
   include CanCan::Ability
 
+  attr_reader :user
+
   def initialize(user)
+    @user = user
+
     if user
       user.admin? ? admin_abilities : user_abilities
     else
@@ -21,5 +27,31 @@ class Ability
 
   def user_abilities
     guest_abilities
+
+    can :create, [Question, Answer, Comment]
+    can :update, [Question, Answer, Comment], user_id: user.id
+    can :destroy, [Question, Answer, Comment], user_id: user.id
+
+    can :like, [Question, Answer] do |item|
+      ratable?(item)
+    end
+
+    can :dislike, [Question, Answer] do |item|
+      ratable?(item)
+    end
+
+    can :unlike, [Question, Answer] do |item|
+      ratable?(item)
+    end
+
+    can :choose_best, Answer do |answer|
+      user.author_of?(answer.question)
+    end
+  end
+
+  def ratable?(item)
+    !user.author_of?(item)
   end
 end
+# rubocop:enable Metrics/MethodLength
+# rubocop:enable Metrics/AbcSize
--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -2,6 +2,7 @@
 
 require 'rails_helper'
 
+# rubocop:disable Metrics/BlockLength
 describe Ability do
   subject(:ability) { Ability.new(user) }
 
@@ -20,4 +21,62 @@ describe Ability do
 
     it { should be_able_to :manage, :all }
   end
+
+  context 'as a general user' do
+    let(:user) { create(:user) }
+    let(:other_user) { create(:user) }
+    let(:question) { create(:question, user: user) }
+    let(:non_author_question) { create(:question, user: other_user) }
+
+    it { should be_able_to :read, :all }
+
+    it { should be_able_to :create, Question }
+    it { should be_able_to :create, Answer }
+    it { should be_able_to :create, Comment }
+
+    it { should be_able_to :update, create(:question, user: user) }
+    it { should be_able_to :update, create(:answer, user: user) }
+    it { should_not be_able_to :update, create(:question, user: other_user) }
+    it { should_not be_able_to :update, create(:answer, user: other_user) }
+
+    it { should be_able_to :destroy, create(:question, user: user) }
+    it { should be_able_to :destroy, create(:answer, user: user) }
+    it { should be_able_to :destroy, create(:comment, user: user) }
+    it { should_not be_able_to :destroy, create(:question, user: other_user) }
+    it { should_not be_able_to :destroy, create(:answer, user: other_user) }
+    it { should_not be_able_to :destroy, create(:comment, user: other_user) }
+
+    it { should be_able_to :like, create(:question, user: other_user) }
+    it { should be_able_to :like, create(:answer, user: other_user) }
+    it { should_not be_able_to :like, create(:question, user: user) }
+    it { should_not be_able_to :like, create(:answer, user: user) }
+
+    it { should be_able_to :dislike, create(:question, user: other_user) }
+    it { should be_able_to :dislike, create(:answer, user: other_user) }
+    it { should_not be_able_to :dislike, create(:question, user: user) }
+    it { should_not be_able_to :dislike, create(:answer, user: user) }
+
+    it { should be_able_to :unlike, create(:question, user: other_user) }
+    it { should be_able_to :unlike, create(:answer, user: other_user) }
+    it { should_not be_able_to :unlike, create(:question, user: user) }
+    it { should_not be_able_to :unlike, create(:answer, user: user) }
+
+    it { should be_able_to :choose_best, create(:answer, question: question) }
+    it { should_not be_able_to :choose_best, create(:answer, question: non_author_question) }
+  end
+
+  describe '#ratable??' do
+    let(:user) { create(:user) }
+    let(:question) { create(:question, user: user) }
+    let(:non_author_question) { create(:question) }
+
+    it 'should return true if user is not author of ratable item' do
+      expect(subject).to be_ratable(non_author_question)
+    end
+
+    it 'should return false if user is author of ratable item' do
+      expect(subject).to_not be_ratable(question)
+    end
+  end
 end
+# rubocop:enable Metrics/BlockLength
