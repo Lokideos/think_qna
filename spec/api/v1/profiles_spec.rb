1a8f0ed3432cc407542e4b925478e82a7e38feed
Lokideos <archmage09@gmail.com>
2019-01-31 20:07:27 +0300

    Fix request test for invalid access token

--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -16,7 +16,7 @@ describe 'Profiles API' do
       end
 
       it 'returns 401 Unauthorized status if access token is invalid' do
-        get '/api/v1/profiles/me', headers: headers
+        get '/api/v1/profiles/me', params: { access_token: '1234' }, headers: headers
         expect(response).to have_http_status 401
       end
     end
