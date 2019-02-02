17b82a15c9a5a164dc70985f5860135cc29a398f
Lokideos <archmage09@gmail.com>
2019-02-02 17:52:39 +0300

    Redefine current_ability instead of current_user in API

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -9,7 +9,7 @@ class Api::V1::BaseController < ApplicationController
     @current_resource_owner ||= User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
   end
 
-  def current_user
-    @current_user ||= current_resource_owner
+  def current_ability
+    @current_ability ||= Ability.new(current_resource_owner)
   end
 end
