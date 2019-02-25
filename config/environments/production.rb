6a47e82d37e29fc31b3cd001227df5cfa1fcfbd6
lokideos <archmage09@gmail.com>
2019-02-25 11:06:20 +0300

    Fix gmail config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,6 +70,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       address:            'smtp.gmail.com',
       port:                            587,
+      domain:          '104.248.89.197:80',
       user_name:      ENV['SMTP_USERNAME'],
       password:       ENV['SMTP_PASSWORD'],
       authentication:              'plain',
